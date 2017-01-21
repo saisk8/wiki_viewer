621cb725b84df541a276106748531e9c2bc63dbd
Sai Somanath.K <k.saisomanath@gmail.com>
2017-01-21 19:26:42 +0530

    Not yet sure code

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
-    Materialize.updateTextFields();
-});
-$('input.autocomplete').autocomplete({
-    data: {
-        "Apple": null,
-        "Microsoft": null,
-        "Google": 'http://placehold.it/250x250'
-    }
-});
+    //When the search icon is clicked
+    $('#search').click(function() {
+        var query = $('#query').val()
+        var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+        var callback = '&callback=JSON_CALLBACK';
+        var url = api + query + callback;
+        console.log(url);
+        $.ajax({
+            type: "GET",
+            url: url,
+            dataType: 'json',
+            success: function(data) {
+                //When the call is a success
+                console.log(data);
+            },
+            error: function(message) {
+                alert('Error');
+            }
 
-//When the search icon is clicked
+        });
+    });
+});
