b3f78524725ca70ba7308822e936e435d6de156b
Sai Somanath.K <k.saisomanath@gmail.com>
2017-02-06 21:12:19 +0530

    New Ajax call

--- js/app.js
+++ js/app.js
@@ -1,25 +1,14 @@
 $(document).ready(function() {
     //When the search icon is clicked
     $('#search').click(function() {
-        var query = $('#query').val()
-        var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
-        var callback = '&callback=JSON_CALLBACK';
-        var url = api + query + callback;
+        var query = $('#query').val();
         $.ajax({
-            type: "GET",
-            url: url,
-            dataType: 'json',
-            success: function(data) {
-                //When the call is a success
-                $('#results').prepend(data);
-
-            },
-            error: function(message) {
-                alert('Request failed, try again for ' + url);
-                $('#results').prepend(data);
-
-            }
-
+            "async": true,
+            "crossDomain": true,
+            "url": "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=" + query;
+            "method": "GET"
+        }).done(function(response) {
+            console.log(response);
         });
     });
 });
