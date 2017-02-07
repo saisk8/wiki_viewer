fd1c2193bb522172877907f820d8e1490f329f44
Sai Somanath.K <k.saisomanath@gmail.com>
2017-02-07 21:06:14 +0530

    New app JS

--- js/app.js
+++ js/app.js
@@ -1,38 +1,13 @@
 $(document).ready(function() {
-    //When the search icon is clicked
-    $('#search').click(function() {
-        var query = $('#query').val();
-        $.ajax({
-            url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + query,
-            dataType: 'jsonp',
-            type: 'POST',
-            headers: {
-                'Api-User-Agent': 'Example/1.0'
-            },
-            success: function(data) {
+    $("#query").val(""); //Empty the search field when the page loads
+    $("#search").on("click", function() {
+        search();
+        $("#random").addClass("hide");
+        $("#text").addClass("hide")
+    })
 
-                // First we clear the children from our class to make sure no previous results are showing.
-                $('#results').empty();
+})
 
-                // Then we also clear the array with the results before providing new information.
-                arrResults.length = 0;
-                var resArr = data.query.search;
+function search() {
 
-                //For each result, generate the html data.
-                for (var result in resArr) {
-                    arrResults.push(new Result(resArr[result].title, resArr[result].snippet));
-                    html = '<div id="articles"><a href="https://en.wikipedia.org/wiki/' + resArr[result].title + '"target="_blank"><h3>' + resArr[result].title + '</h3><p>' + resArr[result].snippet + '</p></a></div>';
-
-                    // Displays the elements to the page
-                    $('#results').append(html);
-                }
-            },
-            error: alert('Not Working');
-        });
-    });
-
-    function Result(title, snippet) {
-        this.title = title;
-        this.snippet = snippet;
-    }
-});
+}
